4a4631c53f47599b9d3ef98475d9c32669c07b11
GabrielEllie <gabarill35@gmail.com>
2024-09-13 09:57:18 -0600

    Created week 2 assignment
    I created student-info component page and a also linked the week2 page in app/page.js

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function StudentInfo() {
+    return(
+        <div>
+            <p>Gabriel Ellison U. Barillos</p>
+            <Link href="https://github.com/GabrielEllie/cprg306-assignments">My Github link</Link>
+        </div>
+    );
+}
